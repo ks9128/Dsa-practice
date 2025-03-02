57645571ca94997e774761e38983f84b9dc4dba6
ks9128 <mdkhalid57470@gmail.com>
2025-03-02 20:03:08 +0530

    lec 69

--- 69 BST/bst.cpp
+++ 69 BST/bst.cpp
@@ -2,6 +2,9 @@
 #include <queue>
 using namespace std;
 
+//# notes : inorder of BST is sorted 
+
+
 class Node {
     public:
         int data;
@@ -17,6 +20,21 @@ class Node {
 
 };
 
+
+
+// preorder
+// NLR 
+void preOrder(Node* root){
+    if (root==NULL) return;
+
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+    
+}
+
+//- main part of bst making
+//! tc is O(log(n)) 
 Node* insertInBst(Node* root, int data){
     // base case
     if(root == NULL){
@@ -65,16 +83,6 @@ void levelOrderTraversal(Node* root){
     }
 
 }
-// preorder
-// NLR 
-void preOrder(Node* root){
-    if (root==NULL) return;
-
-    cout<<root->data<<" ";
-    preOrder(root->left);
-    preOrder(root->right);
-    
-}
 
 void insertElement(Node* &root){
     
@@ -85,6 +93,25 @@ void insertElement(Node* &root){
         root = insertInBst(root,data);
         cin>> data;
     }
+}
+
+//- min element of bst
+Node* minVal(Node* root){
+    Node* temp = root;
+    while(temp->left != NULL){
+        temp = temp->left;
+    }
+
+    return temp;
+}
+
+//- max element of bst
+Node* maxVal(Node* root){
+    Node* temp = root;
+    while(temp->right != NULL){
+        temp = temp->right;
+    }
+    return temp;
 }
 int main(){
 
@@ -99,4 +126,9 @@ int main(){
 
     cout<<"preorder traversal: ";
     preOrder(root);
+
+    cout<<endl;
+
+    cout<<"min value is:" <<minVal(root)->data << endl;
+    cout<< "max vlaue is :" << maxVal(root)->data << endl;
 }
