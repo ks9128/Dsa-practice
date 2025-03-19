8d73e74d42a8004100dc8780c07be9ddf82a0cd5
ks9128 <mdkhalid57470@gmail.com>
2025-03-19 23:26:09 +0530

    lec 87 done

---
+++ lec 87/dfs.cpp
@@ -0,0 +1,42 @@
+#include<unordered_map>
+#include<list>
+#include<iostream>
+using namespace std;
+
+void dfs(int node,unordered_map<int,bool> &visited, unordered_map<int,list<int> > &adj,
+        vector<int> &component){
+            component.push_back(node);
+            visited[node] = true;
+
+            //hr connected node k liye recursive call
+            for(auto i: adj[node]){
+                if(!visited[i]){
+                    dfs(i,visited,adj,component);
+                }
+            }
+        }
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+{
+    //create adj list
+    unordered_map<int,list<int> > adj;
+    for(int i=0; i<edges.size(); i++){
+        int u= edges[i][0];
+        int v= edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<vector<int>> ans;
+    unordered_map<int,bool> visited;
+    //becuz it van have multiple components
+    for(int i=0; i<V; i++){
+        if(!visited[i]){
+            vector<int>component;
+            dfs(i,visited,adj,component);
+            ans.push_back(component);
+        }
+    }
+    return ans;
+    
+}
