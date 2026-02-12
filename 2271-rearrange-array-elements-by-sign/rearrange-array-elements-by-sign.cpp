107c80f7738d645a99645b5ecfd1ef9d0a39c82d
Khalid Saifullah <150775356+ks9128@users.noreply.github.com>
2026-02-13 01:07:22 +0530

    Time: 2 ms (88.89%) | Memory: 128.3 MB (56.13%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,22 +1,44 @@
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) {
+//         vector<int> postive;
+//         vector<int> negetive;
+
+//         for(int i=0; i<nums.size();i++){
+//             if(nums[i]<0){
+//                 negetive.push_back(nums[i]);
+//             }
+//             else{
+//                 postive.push_back(nums[i]);
+//             }
+//         }
+//         int j=0;
+//         for(int i=0; i<postive.size();i++){
+//             nums[j++] = postive[i];
+//             nums[j++] = negetive[i];
+//         }
+//         return nums;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> postive;
-        vector<int> negetive;
+        vector<int>ans(nums.size(),0);
+        
+        
+        int pos=0,neg=1;
+        
 
-        for(int i=0; i<nums.size();i++){
-            if(nums[i]<0){
-                negetive.push_back(nums[i]);
-            }
-            else{
-                postive.push_back(nums[i]);
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                ans[pos] = nums[i];
+                pos+=2;
+            }else{
+                ans[neg] = nums[i];
+                neg+=2;
             }
         }
-        int j=0;
-        for(int i=0; i<postive.size();i++){
-            nums[j++] = postive[i];
-            nums[j++] = negetive[i];
-        }
-        return nums;
+        return ans;
     }
 };
